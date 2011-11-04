6359905681f57f7969d9c9102273f591f42c3152
hening malam <jedi_biru@yahoo.co.uk>
2011-11-04 14:57:02 +0700

    add home panel

---
+++ js/view/homepanel.js
@@ -0,0 +1,8 @@
+jun.homePanel = Ext.extend(Ext.Panel ,{    
+    html:'<p style="padding:25px">welcome to the jungle</p>',
+    initComponent: function(){
+        jun.homePanel.superclass.initComponent.call(this);
+    }
+})
+
+
